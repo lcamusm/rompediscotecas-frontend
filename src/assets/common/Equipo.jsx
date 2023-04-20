90c1590187cc4a35df348b944a7a359ba7466f01
Vicente <verrazurizl@uc,cl>
2023-04-20 15:51:54 -0400

    Navegacion lista

---
+++ src/assets/common/Equipo.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header"
+
+function Equipo() {
+    return (
+        <>
+        <Header/>
+        <h1>Equipo</h1>
+        </>
+    )
+}
+
+export default Equipo
---
+++ src/assets/game/Instructions.jsx
@@ -0,0 +1,13 @@
+import Header from "../common/Header";
+
+function Instructions(props) {
+  return (
+    <div className="instructions">
+        <Header/>
+        <h2>Instructions</h2>
+        <p>Click on an image to earn points, but don't click on any more than once!</p>
+    </div>
+  )
+}
+
+export default Instructions;
