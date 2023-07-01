236dea8962cd39a5973c3a38bc5bd50b9b617bec
LUCAS CAMUS <lcamusm@uc.cl>
2023-07-01 11:25:51 -0400

    waiting room listo

--- src/assets/common/Header.jsx
+++ src/assets/common/Header.jsx
@@ -8,12 +8,12 @@ function Header() {
     <header>
       <img src={logo} className="logo"/>
       <a href="/">Inicio</a>
-      <a href="/login">Log in</a>
-      <a href="/signup">Sign up</a>
       <a href="/el-juego">El juego</a>
       <a href="/instrucciones">Instrucciones</a>
-      <a href="/nosotros">Nosotros</a>
       <a href="/play">Jugar</a>
+      <a href="/login">Log in</a>
+      <a href="/signup">Sign up</a>
+      <a href="/nosotros">Nosotros</a>
       <br></br>
       <LogoutButton />
     </header>
